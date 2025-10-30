a4d7caf21508980b215b3ef34da7236a5f9f082c
emmituwu <emmajazseo@gmail.com>
2025-10-30 13:08:25 -0300

    nuevo projecto uwu

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='text-5xl
+    bg-mint-500'>
+      <h1 className='font-circular'>Welcome to Awards ajshdhasjkhdj me gusta daniel</h1>
+      <div className='bg-mint-500 font-general'>
+        holaaaaaaaaaa jajaja
+      </div>
+    </main>
+  )
+}
+
+export default App
