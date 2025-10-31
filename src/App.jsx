c72dd1ee63089fed26a7cbce9e4e28bd7f7ce93e
emmituwu <emmajazseo@gmail.com>
2025-10-31 13:25:42 -0300

    terminando configurar los customs

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <main className='text-5xl
-    bg-mint-500'>
-      <h1 className='font-circular'>Welcome to Awards ajshdhasjkhdj me gusta daniel</h1>
-      <div className='bg-mint-500 font-general'>
-        holaaaaaaaaaa jajaja
-      </div>
+    <main>
+      <h1 className='text-5xl text-blue-50 font-bold font-circular'>Welcome to Awards ajshdhasjkhdj me gusta daniel</h1>
     </main>
   )
 }
