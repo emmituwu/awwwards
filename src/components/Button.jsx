eec7130394835131f698b3883fb930e4d7b0ae21
emmituwu <emmajazseo@gmail.com>
2026-04-08 11:08:09 -0300

    terminada la hero sectionn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = (title, id, PiMouseRightClickDuotone, PiMouseLeftClickDuotone, containerClass) => {
+const Button = ({ title, id, leftIcon, rightIcon, containerClass }) => {
   return (
-    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass}`}>
         {leftIcon}
 
         <span className='relative incline-flex overflow-hidden font-general text-xs uppercase'>
@@ -10,6 +10,7 @@ const Button = (title, id, PiMouseRightClickDuotone, PiMouseLeftClickDuotone, co
                 {title}
             </div>
         </span>
+        {rightIcon}
     </button>
   )
 }
